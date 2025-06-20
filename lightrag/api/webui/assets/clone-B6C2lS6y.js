0e2737e68156e2b0cc3b4dc4ed604177014e8d4a
Boris Hnila <boris.hnila@gmail.com>
2025-06-20 10:35:26 +0200

    Fixed base path of login request

--- lightrag/api/webui/assets/clone-B6C2lS6y.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./_baseUniq-Duc94Gro.js";var e=4;function a(o){return r(o,e)}export{a as c};
---
+++ lightrag/api/webui/assets/clone-nU4XJN_8.js
@@ -0,0 +1,1 @@
+import{b as r}from"./_baseUniq-DXFv_NWp.js";var e=4;function a(o){return r(o,e)}export{a as c};
